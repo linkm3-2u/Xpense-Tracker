27d2a481f8af55b8e65abda799113f28c4d79c5f
linkm3-2u <sagarvarun314@gmail.com>
2024-06-07 18:06:58 +0530

    Checking if commit works properly..changing - to --

--- Xpensr/src/Components/Home.jsx
+++ Xpensr/src/Components/Home.jsx
@@ -4,7 +4,7 @@ import { Typography, Container } from "@mui/material";
 const Home = () => {
   return (
     <Container maxWidth="md" style={{ marginTop: "50px", textAlign: "center" }}>
-      <Typography variant="h4" gutterBottom>Welcome to Expense Tracker – Your Financial Companion!</Typography>
+      <Typography variant="h4" gutterBottom>Welcome to Expense Tracker –-- Your Financial Companion!</Typography>
       <Typography variant="body1">
         Catch a glimpse of your financial journey with My Expense Tracker. Easily track transaction history, manage total balance, and categorize expenses, all in one place. Simplify your financial management and stay on top of your spending effortlessly.
       </Typography>
